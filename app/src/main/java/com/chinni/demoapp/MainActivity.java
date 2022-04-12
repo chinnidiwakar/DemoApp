2b0a8181c8a4d5eb0d0c8982a6f5fbae1ae471e8
Falcon Rising <chinnidiwakar5@gmail.com>
2022-04-12 19:56:35 +0530

    update

--- app/src/main/java/com/chinni/demoapp/MainActivity.java
+++ app/src/main/java/com/chinni/demoapp/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String sdk;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sdk = "12042022";
     }
 }
